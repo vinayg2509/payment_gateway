f154a0f76e2b7bbd9e128d4a9af8024b5d86db80
vinayg2509 <vinayg2509@gmail.com>
2025-07-08 19:25:55 +0530

    [Vinay]Add Payment class with validation and async payment processing logic

---
+++ payments/Payment.js
@@ -0,0 +1,23 @@
+class Payment {
+  constructor(userName, amountToBePaid) {
+    this.userName = userName;
+    this.amountToBePaid = amountToBePaid;
+    this.dateAndTime = new Date().toDateString()
+  }
+
+  validate()
+  {
+    throw new Error("Subclasses must implement validate()");
+  }
+  paymentProcess(callBack) {
+    console.log(`⏳ Processing payment of ₹${this.amountToBePaid} for ${this.userName}...`);
+
+    setTimeout(() => {
+      const isSuccessfull = Math.random() > 0.2; // 80% success rate
+      callBack(isSuccessfull);
+    }, 2000);
+  }
+}
+
+
+module .export= Payment
