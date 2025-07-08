acd4853025f540e988393e1f05884c484e2737db
vinayg2509 <vinayg2509@gmail.com>
2025-07-08 23:11:50 +0530

    [Vinay]Corrected typos

--- payments/Payment.js
+++ payments/Payment.js
@@ -2,21 +2,23 @@ class Payment {
   constructor(userName, amountToBePaid) {
     this.userName = userName;
     this.amountToBePaid = amountToBePaid;
-    this.dateAndTime = new Date().toDateString()
+    this.dateAndTime = new Date().toDateString(); // Records payment date
   }
 
-  validate()
-  {
+  // Method to be implemented by subclasses
+  validate() {
     throw new Error("Subclasses must implement validate()");
   }
-  paymentProcess(callBack) {
+
+  // Simulates asynchronous payment processing
+  paymentProcess(callback) {
     console.log(`⏳ Processing payment of ₹${this.amountToBePaid} for ${this.userName}...`);
 
     setTimeout(() => {
-      const isSuccessfull = Math.random() > 0.2; // 80% success rate
-      callBack(isSuccessfull);
+      const isSuccessful = Math.random() > 0.2; // 80% success rate
+      callback(isSuccessful);
     }, 2000);
   }
 }
 
-export default Payment
+export default Payment;
