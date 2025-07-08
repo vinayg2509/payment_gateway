65b3701e9e1ec1d4196e01a891238eef1cde9bf1
vinayg2509 <vinayg2509@gmail.com>
2025-07-08 23:11:01 +0530

    [Vinay]Corrected typos

--- payments/CardPayment.js
+++ payments/CardPayment.js
@@ -1,16 +1,15 @@
-    import Payment from './Payment.js'
-    class CardPayment extends Payment
-    {
-        constructor(userName,amountToBePaid,cardNumber,paymentMode)
-        {
-            super(userName, amountToBePaid) 
-            this .cardNumber=cardNumber
-            this.paymentMode=paymentMode
-        }
-        validate()
-        {
-            return /^\d{16}$/.test(this.cardNumber)
-        }
-    }
+import Payment from './Payment.js';
 
-export default CardPayment
+class CardPayment extends Payment {
+  constructor(userName, amountToBePaid, cardNumber) {
+    super(userName, amountToBePaid);
+    this.cardNumber = cardNumber;
+    this.paymentMode = "card"; // Set internally
+  }
+
+  validate() {
+    return /^\d{16}$/.test(this.cardNumber); // Must be 16 digits
+  }
+}
+
+export default CardPayment;
