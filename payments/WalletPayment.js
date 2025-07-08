641134a9faa9c16574f768b3f3ba5e098335c87f
vinayg2509 <vinayg2509@gmail.com>
2025-07-08 20:17:47 +0530

    [Vinay]Add WalletPayment subclass with wallet ID validation and payment mode setup

---
+++ payments/WalletPayment.js
@@ -0,0 +1,15 @@
+    import Payment from './Payment.js'
+    class WalletPayment extends Payment
+    {
+        constructor(userName,amountToBePaid,walletId)
+        {
+            super(userName, amountToBePaid) 
+            this .walletId=walletId
+            this.paymentMode="wallet"
+        }
+        validate()
+        {
+            return this.walletId.length===5
+        }
+    }
+
